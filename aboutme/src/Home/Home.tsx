b1378719068fdfb6eec894aaa3e51ec307a94125
carlnoia96 <carlokreosoft@gmail.com>
2022-07-24 20:09:00 +0200

    inizio primi 2 blocchi

--- aboutme/src/Home/Home.tsx
+++ aboutme/src/Home/Home.tsx
@@ -1,8 +1,11 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 import './Home.css';
 
-interface HomeProps {}
-interface HomeState {}
+interface HomeProps {
+}
+
+interface HomeState {
+}
 
 /*const Home: FC<HomeProps> = () => (
   <div className={styles.Home}>
@@ -10,11 +13,31 @@ interface HomeState {}
   </div>
 );*/
 
-class Home extends React.Component<HomeProps, HomeState>{
+class Home extends React.Component<HomeProps, HomeState> {
     render() {
         return (
-            <div className={"home"}>
-                Home Component
+            <div>
+                <div className={"start full-screen"}>
+                    <div className={"title text-align-center"}>
+                        <p>Ciao, sono <span className={"red"}>Ambra</span><br/><br className={"hide-on-desktop"}/>
+                            <span className={"bold"}> interior & social <br className={"hide-on-desktop"}/> designer specialist</span>
+                        </p>
+                    </div>
+                    <div className={"image"}>
+                        <div className={"img"}/>
+                    </div>
+                </div>
+                <div className={"block-2"}>
+                    <div className={"image"}>
+                        <div className={"img"}/>
+                    </div>
+                    <div className={"text text-align-center"}>
+                        <p>
+                            Fin da così piccina avevo le idee chiarissime su quello che avrei voluto fare da grande:
+                            volevo essere una <br/> <span className={"red"}>Interior designer di successo!</span>
+                        </p>
+                    </div>
+                </div>
             </div>
         );
     }
