992d3367fbd1ec4aebb3cf33726afb4d34bc53da
CarloNoia <carlokreosoft@gmail.com>
2022-07-29 15:27:16 +0200

    inizio blocco 4

--- aboutme/src/Home/Home.tsx
+++ aboutme/src/Home/Home.tsx
@@ -47,23 +47,60 @@ class Home extends React.Component<HomeProps, HomeState> {
                         <div className={"text-box"}>
                             <div className={"container bk-red white text-align-center"}>
                                 <p>
-                                    Negli anni io crescevo ed insieme a me anche il mio sogno si veniva a delineare sempre di più.
+                                    Negli anni io crescevo ed insieme a me anche il mio sogno si veniva a delineare
+                                    sempre di più.
                                 </p>
                                 <p>
-                                    La mia situazione economica non mi consentì di intraprendere questo percorso. Iniziai così a lavorare come commessa di abbigliamento in un centro commerciale.
+                                    La mia situazione economica non mi consentì di intraprendere questo percorso.
+                                    Iniziai così a lavorare come commessa di abbigliamento in un centro commerciale.
                                 </p>
                             </div>
                         </div>
                         <div className={"text-box bk-gary"}>
                             <div className={"container bk-darkGray white text-align-center"}>
                                 <p>
-                                    Nonostante il lavoro impegnasse gran parte delle mie giornate, sentivo la necessità di continuare a studiare e allora mi iscrissi all'università di Economia, che era nella mia città e andai avanti di borse di studio.
+                                    Nonostante il lavoro impegnasse gran parte delle mie giornate, sentivo la necessità
+                                    di continuare a studiare e allora mi iscrissi all'università di Economia, che era
+                                    nella mia città e andai avanti di borse di studio.
                                 </p>
                                 <p>
                                     Da lì a poco compresi che il mio sogno non era del tutto irrealizzabile...
                                 </p>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div className={"block-4"}>
+                    <div className={"container-1"}>
+                        <p className={"text-align-center"}>
+                            Mi sono laureata in Economia e gestione delle imprese in pieno Covid-19 sviluppando la tesi
+                            sulle strategie per l'acquisizione e il mantenimento del potere di mercato.
+                        </p>
+                        <p className={"text-align-center"}>
+                            Ho maturato una spiccata propensione per l'<span
+                            className={"bold"}>imprenditorialità</span> ed un conseguente avvicinamento
+                            allo studio del <span className={"bold"}>Digital Marketing</span>.
+                        </p>
+                        <p className={"text-align-center"}>
+                            Conseguita la Laurea vedevo aprirsi <br/>davanti a me un'idea: <br/>diventare imprenditrice.<br/>
+                            Sì, ma di che cosa?
+                        </p>
+                        <p className={"text-align-center red"}>
+                            Imprenditrice della mia passione!
+                        </p>
+                    </div>
+                    <div className={"container-2"}>
+                        <p className={"text-align-center"}>
+                            Grazie al supporto del mio fidanzato, dopo quasi setti anni ho capito che era giunto il
+                            momento di cambiare vita. Volevo <span className={"red"}>sentirmi</span> finalmente <span
+                            className={"red"}>realizzata</span>!
+                        </p>
+                        <p className={"text-align-center"}>
+                            Non ne potevo più di tornare a casa sempre priva di energia ed entusiasmo e di avere la vita
+                            sempre gestita da qualcun altro, così ho deciso di lasciare il posto fisso per <span
+                            className={"red"}>inseguire il
+                            sogno di una vita</span>.
+                        </p>
                     </div>
                 </div>
             </div>
